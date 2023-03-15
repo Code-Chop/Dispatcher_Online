32dc847ceadf660465d4859afbfe27654b51d27a
Naveen <naveenkbs2002@gmail.com>
2023-03-15 21:47:45 +0530

    current commits

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./flaskr/templates/**/*.{html}",
+  "./flaskr/static/**/*.{css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
